ea63abe494b08b735cb8a8f3c6b16e7aca630416
fajiaopaopao <wentu1996@163.com>
2019-12-23 18:58:00 +0800

    feat: 实例样式修改(#20191127_upgrade)

--- examples/components/Buttons.vue
+++ examples/components/Buttons.vue
@@ -1,48 +1,46 @@
 <template>
   <div class="rui-flex">
-    <header class="rui-header rui-header-loan">
-      <span class="back" @click="jumpUrl"><i class="rui-ico">&#xe902;</i></span>
+    <header class="rui-header rui-header-vue-lego">
+      <i class="rui-ico back"  @click="jumpUrl">&#xe902;</i>
       <h2 class="title">Button</h2>
-    </header>
+    </header> 
     <div class="bd flex">
-      <ul class="rui-list">
-        <li class="item title">按钮类型</li>
-        <div class="btns-wrap">
-          <r-button>default</r-button>
-          <r-button type="primary">primary</r-button>
-          <r-button type="success">success</r-button>
-          <r-button type="primary-ghost">primary-ghost</r-button>
-          <r-button type="success-ghost">success-ghost</r-button>
-        </div>
-        <li class="item title">禁用按钮</li>
-        <div class="btns-wrap">
-          <r-button disabled>default</r-button>
-          <r-button disabled type="primary">primary</r-button>
-          <r-button disabled type="success">success</r-button>
-          <r-button disabled type="primary-ghost">primary-ghost</r-button>
-          <r-button disabled type="success-ghost">success-ghost</r-button>
-        </div>
-        <li class="item title">形状按钮</li>
-        <div class="btns-wrap">
-          <r-button shape="round" type="primary">椭圆按钮</r-button>
-          <r-button shape="round" type="success-ghost">椭圆按钮</r-button>
-        </div>
-        <li class="item title">按钮尺寸</li>
-        <div class="btns-wrap">
-          <r-button shape="round" size="large">large按钮</r-button>
-          <r-button size="long">long按钮</r-button>
-          <r-button type="primary" size="medium">medium按钮</r-button>
-        </div>
-        <li class="item title">加载状态</li>
-        <div class="btns-wrap">
-          <r-button type="primary-ghost" loading>加载状态</r-button>
-        </div>
-        <li class="item title">路由跳转</li>
-        <div class="btns-wrap">
-          <r-button type="primary-ghost" url="https://www.baidu.com">url跳转</r-button>
-          <r-button type="success-ghost" to="/">router跳转</r-button>
-        </div>
-      </ul>
+      <p class="g-title">按钮类型</p>
+      <div class="btns-wrap">
+        <r-button>default</r-button>
+        <r-button type="primary">primary</r-button>
+        <r-button type="success">success</r-button>
+        <r-button type="primary-ghost">primary-ghost</r-button>
+        <r-button type="success-ghost">success-ghost</r-button>
+      </div>
+      <p class="g-title">禁用按钮</p>
+      <div class="btns-wrap">
+        <r-button disabled>default</r-button>
+        <r-button disabled type="primary">primary</r-button>
+        <r-button disabled type="success">success</r-button>
+        <r-button disabled type="primary-ghost">primary-ghost</r-button>
+        <r-button disabled type="success-ghost">success-ghost</r-button>
+      </div>
+      <p class="g-title">形状按钮</p>
+      <div class="btns-wrap">
+        <r-button shape="round" type="primary">椭圆按钮</r-button>
+        <r-button shape="round" type="success-ghost">椭圆按钮</r-button>
+      </div>
+      <p class="g-title">按钮尺寸</p>
+      <div class="btns-wrap">
+        <r-button shape="round" size="large">large按钮</r-button>
+        <r-button size="long">long按钮</r-button>
+        <r-button type="primary" size="medium">medium按钮</r-button>
+      </div>
+      <p class="g-title">加载状态</p>
+      <div class="btns-wrap">
+        <r-button type="primary-ghost" loading>加载状态</r-button>
+      </div>
+      <p class="g-title">路由跳转</p>
+      <div class="btns-wrap">
+        <r-button type="primary-ghost" url="https://www.baidu.com">url跳转</r-button>
+        <r-button type="success-ghost" to="/">router跳转</r-button>
+      </div>
     </div>
   </div>
 </template>
@@ -66,15 +64,21 @@ export default class Buttons extends Vue {
     background-color: #fff;
   }
   .bd{
-    overflow-y: scroll;
-    .title{
-      font-weight: 800;
-      padding: .1rem 0 0 0;
-      color: #4f8cf0;
-    }
-    .rui-list {
-      margin-right: .1rem;
-    }
+    padding: .2rem;
+  }
+  .flex{
+    background-color: #fff;
+  }
+  .bd{
+    // overflow-y: scroll;
+    // .title{
+    //   font-weight: 800;
+    //   padding: .1rem 0 0 0;
+    //   color: #4f8cf0;
+    // }
+    // // .rui-list {
+    //   margin-right: .1rem;
+    // }
     .btns-wrap {
       margin: .1rem 0 .2rem 0;
       > button {
