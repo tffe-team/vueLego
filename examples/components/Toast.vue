83bed6fb5113578893f0eab3ac4fa9a2dfc5e578
wenbin <--help>
2020-01-13 18:16:01 +0800

    feat: toast 演示样式修改(#20191127_upgrade)

--- examples/components/Toast.vue
+++ examples/components/Toast.vue
@@ -3,18 +3,18 @@
     <div class="flex bd">
       <h3 class="g-title">基础用法</h3>
       <div class="btn-box">
-        <div class="rui-btn g-btn g-success-btn" @click="onShortClick">单行文字</div>
-        <div class="rui-btn g-btn g-success-btn" @click="onLongClick">多行文字</div>
+        <r-button type="primary" @click="onShortClick">单行文字</r-button>
+        <r-button type="primary" @click="onLongClick">多行文字</r-button>
       </div>
       <h3 class="g-title">通知类型</h3>
       <div class="btn-box">
-        <div class="rui-btn g-btn g-success-btn" @click="onInfoClick">普通通知</div>
-        <div class="rui-btn g-btn" @click="onSuccessClick">成功通知</div>
-        <div class="rui-btn g-btn g-error-btn" @click="onErrorClick">错误通知</div>
+        <r-button type="primary" @click="onInfoClick">普通通知</r-button>
+        <r-button type="success" @click="onSuccessClick">成功通知</r-button>
+        <r-button type="error" @click="onErrorClick">错误通知</r-button>
       </div>
       <h3 class="g-title">自定义配置</h3>
       <div class="btn-box">
-        <div class="rui-btn g-btn" @click="onCloseEventClick">自定义关闭回调</div>
+        <r-button type="success" @click="onCloseEventClick">自定义关闭回调</r-button>
       </div>
     </div>
   </div>
