fea6b3f79cb9736363c65f3036b9d9b987da4435
wangdongjie <wangdongjie@shiguangjk.com>
2020-02-07 18:58:50 +0800

    dialog

--- src/components/dialog/__test__/dialog.spec.js
+++ src/components/dialog/__test__/dialog.spec.js
@@ -1,4 +1,4 @@
-import {config} from '@vue/test-utils'
+import {mount, config} from '@vue/test-utils'
 import $dialog from '../index'
 import {later} from '../../../test'
 
@@ -27,4 +27,24 @@ test('dialog type', async () => {
   await later()
   expect(document.querySelector(selector)).toMatchSnapshot()
   $dialog.remove()
+})
+
+test('title prop', async ()=> {
+  $dialog.info({
+    title: 'this is a title',
+    content: 'this is a content'
+  })
+  await later()
+  expect(document.querySelector('.title')).toMatchSnapshot()
+  $dialog.remove()
+})
+
+test('button click', async ()=> {
+  const dialog = $dialog.info({
+    title: 'this is a title',
+    content: 'this is a content'
+  })
+  const wrapper = mount($dialog)
+  wrapper.find('vue-lego-btn-dialog').trigger('click')
+  expect(wrapper.emitted().click).toBeTruthy()
 })
