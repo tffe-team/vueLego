aca9f2928d93ba0ce414fe89f4c0ce8f21c92bd1
houshiduo <houshiduo@shiguangjk.com>
2020-02-18 19:29:01 +0800

    test: dialog unit  test

--- src/components/dialog/confirm.ts
+++ src/components/dialog/confirm.ts
@@ -64,24 +64,41 @@ DialogComponent.newInstance = properties => {
       }
     },
     render (h: any){
-      return h(Dialog, {
-        props: Object.assign({}, this.$data),
-        domProps: {
-          value: this.visible
-        },
-        on: {
-          'input': (status) => {
-            this.visible = status
+      if (this.$data.render) {
+        return h(Dialog, {
+          props: Object.assign({}, this.$data),
+          domProps: {
+            value: this.visible
           },
-          'on-cancel': this.cancel,
-          'on-close': this.close,
-          'on-ok': this.ok
-        },
-      }, [h(contentRender, {
-        props: {
-          render:  this.$data.render
-        }
-      }), this.$slots.default])
+          on: {
+            'input': (status) => {
+              this.visible = status
+            },
+            'on-cancel': this.cancel,
+            'on-close': this.close,
+            'on-ok': this.ok
+          },
+        }, [h(contentRender, {
+          props: {
+            render: this.$data.render
+          }
+        }), this.$slots.default])
+      } else {
+        return h(Dialog, {
+          props: Object.assign({}, this.$data),
+          domProps: {
+            value: this.visible
+          },
+          on: {
+            'input': (status) => {
+              this.visible = status
+            },
+            'on-cancel': this.cancel,
+            'on-close': this.close,
+            'on-ok': this.ok
+          },
+        })
+      }
     },
   });
   const component = Instance.$mount();
--- src/test/spec/dialog.spec.js
+++ src/test/spec/dialog.spec.js
@@ -10,14 +10,14 @@ Vue.prototype.$LEGO = {
 
 const selector = '.r-vue-lego-dialog'
 
-// test('create a dialog', async () => {
-//   $dialog.info({
-//     content: 'this is a dialog'
-//   })
-//   await later()
-//   expect(document.body.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('create a dialog', async () => {
+  $dialog.info({
+    content: 'this is a dialog'
+  })
+  await later()
+  expect(document.body.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
 test('create a render dialog', async () => {
   $dialog.confirm({
@@ -43,92 +43,79 @@ test('create a render dialog', async () => {
   $dialog.remove()
 })
 
-// test('dialog type', () => {
-//   $dialog.success({
-//     content: 'this is a success dialog'
-//   })
-//   expect(document.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('dialog type', () => {
+  $dialog.success({
+    content: 'this is a success dialog'
+  })
+  expect(document.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
-// test('title prop', ()=> {
-//   $dialog.info({
-//     title: 'this is a title',
-//     content: 'this is a content'
-//   })
-//   expect(document.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('title prop', ()=> {
+  $dialog.info({
+    title: 'this is a title',
+    content: 'this is a content'
+  })
+  expect(document.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
-// test('show closeAble', ()=> {
-//   $dialog.info({
-//     closeAble: true,
-//     content: 'i will show closeAble'
-//   })
-//   expect(document.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('show closeAble', ()=> {
+  $dialog.info({
+    closeAble: true,
+    content: 'i will show closeAble'
+  })
+  expect(document.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
-// test('hide footer', ()=> {
-//   $dialog.info({
-//     footerHide: true,
-//     content: 'i will hide footer'
-//   })
-//   expect(document.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('hide footer', ()=> {
+  $dialog.info({
+    footerHide: true,
+    content: 'i will hide footer'
+  })
+  expect(document.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
-// test('hide mask', () => {
-//   $dialog.info({
-//     showMask: false,
-//     content: 'i will hide mask'
-//   })
-//   expect(document.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('hide mask', () => {
+  $dialog.info({
+    showMask: false,
+    content: 'i will hide mask'
+  })
+  expect(document.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
-// test('show cancel', () => {
-//   $dialog.info({
-//     showCancel: true,
-//     content: 'i will show cancel'
-//   })
-//   expect(document.querySelector(selector)).toMatchSnapshot()
-//   $dialog.remove()
-// })
+test('show cancel', () => {
+  $dialog.info({
+    showCancel: true,
+    content: 'i will show cancel'
+  })
+  expect(document.querySelector(selector)).toMatchSnapshot()
+  $dialog.remove()
+})
 
-// test('ok button click', ()=> {
-//   const onOk = jest.fn()
-//   $dialog.info({
-//     content: 'this is ok button click',
-//     onOk
-//   })
-//   document.querySelector('.vue-lego-btn-dialog:last-child').click()
-//   expect(onOk).toHaveBeenCalledTimes(1)
-//   $dialog.remove()
-// })
+test('ok button click', ()=> {
+  const onOk = jest.fn()
+  $dialog.info({
+    content: 'this is ok button click',
+    onOk
+  })
+  document.querySelector('.vue-lego-btn-dialog:last-child').click()
+  expect(onOk).toHaveBeenCalledTimes(1)
+  $dialog.remove()
+})
 
 // /** 这个cancel只有在confirm模式下生效 */
-// test('cancel button click', ()=> {
-//   const onCancel = jest.fn()
-//   $dialog.confirm({
-//     content: 'this is cancel button click',
-//     showCancel: true,
-//     onCancel
-//   })
-//   document.querySelector('.vue-lego-btn-dialog:first-child').click()
-//   expect(onCancel).toHaveBeenCalledTimes(1)
-//   $dialog.remove()
-// })
-
-/** 这个close方法有问题 */
-// test('close button click', async ()=> {
-//   const onClose = jest.fn()
-//   $dialog.info({
-//     closeAble: true,
-//     content: 'this is close button click',
-//     onClose
-//   })
-//   document.querySelector('.close-btn').click()
-//   expect(onClose).toHaveBeenCalledTimes(1)
-//   $dialog.remove()
-// })
+test('cancel button click', ()=> {
+  const onCancel = jest.fn()
+  $dialog.confirm({
+    content: 'this is cancel button click',
+    showCancel: true,
+    onCancel
+  })
+  document.querySelector('.vue-lego-btn-dialog:first-child').click()
+  expect(onCancel).toHaveBeenCalledTimes(1)
+  $dialog.remove()
+})
